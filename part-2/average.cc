45a375f051cf28acdfc2de58f152a7d61fdb8b6d
VINNY2x <143225758+VINNY2x@users.noreply.github.com>
2023-11-08 18:47:38 +0000

    average.cc

--- part-2/average.cc
+++ part-2/average.cc
@@ -9,20 +9,20 @@
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
-  if ((arguments.size() -1) < 1) {
+  if ((arguments.size() - 1) < 1) {
     std::cout << "error: you must supply at least one number";
     return 1;
   }
   double add_up = 0.0;
   bool first_index = true;
-  for (std::string val : arguments) {
+  for (std::string& val : arguments) {
     if (first_index) {
       first_index = false;
       continue;
     }
     add_up += std::stod(val);
   }
-  double divide = arguments.size() - 1;
+  int divide = static_cast<int>(arguments.size()) - 1;
   double result = add_up / divide;
   std::cout << "average = " << result;
   return 0;
